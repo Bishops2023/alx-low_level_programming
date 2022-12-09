e9c4ce429df7c29fa359e84f533ca1d3c5d37a70
Bishops2023 <Jonahinnocent463@gmail.com>
2022-12-08 21:03:04 -0800

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main-Entry
+ *Return:Always
+ */
+int main(void)
+{
+	printf("size of a char: %1 byte(s)\n", sizeof(char));
+	printf("size of an int: %1 byte(s)\n", sizeof(init));
+	printf("size of a long init: %1 byte(s)\n", sizeof(long init));
+	print("size of a long long init: %1 byte(s)\n", sizeof(long long init));
+	print("size of a float: %1 byte(s)\n", sizeof(float));
+	return (0);
+};
