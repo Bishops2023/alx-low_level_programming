60f8a9013cb0311c36eced2386b62e064c945e73
Bishops2023 <Jonahinnocent463@gmail.com>
2022-12-15 17:42:43 -0800

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+int _putchar (char c);
+	void print _alphabet (void);
+	void print _ alphabet _10(void);
+	int _islower (int c);
+	int _ isalpha (int c);
+	int print _  sign (int n);
+	int _abs (int);
+	int print _ last _ digit (int);
+	void jack _ bailer (void);
+	void times table (void);
+	int add (int, int);
+	void print _ to _ 98 (int n);
+	void print _ times _ table (int n);
+
