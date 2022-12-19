00210aedcf566bed797207c9038bf9e3022442b5
Bishops2023 <Jonahinnocent463@gmail.com>
2022-12-19 02:39:25 -0800

    task0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,10 @@
-MAIN.H file
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(Char c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,5 +14,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
- char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
+#endif /* MAIN_H */
